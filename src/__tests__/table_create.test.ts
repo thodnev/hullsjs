5589985b6bfdd27fa897cbb5978338626944f031
thodnev <thodnev@gmail.com>
2024-10-04 04:09:51 +0200

    test(HullsDB.table_create): rewrite to jest (WIP)

--- src/__tests__/table_create.test.ts
+++ src/__tests__/table_create.test.ts
@@ -4,19 +4,96 @@
 
 import h from '..'
 
-const db = new h.HullsDB('mydb');
-db.create_table({name: 'nokey'});
-db.create_table({name: 'nokeynoinc', autoinc: false});
-db.create_table({name: 'nokey+inc', autoinc: true});
-db.create_table({name: 'str', key: 'noinchere'});
-db.create_table({name: 'strnoinc', key: 'dontincme', autoinc: false});
-db.create_table({name: 'str+inc', key: 'incme', autoinc: true});
-db.create_table({name: 'arr[]', key: []});
-db.create_table({name: 'arr[..]', key: ['one', 'two']});
-db.create_table({name: 'arr[..]+inc', key: ['one', 'two'], autoinc: true});
-db.create_table({name: 'arr[.., AUTOINC]', key: ['one', 'two', h.AUTOINC]});
-db.create_table({name: 'AUTOINC', key: h.AUTOINC});
+
+describe('Create tables', () => {
+    let db
+
+    function last_entry() {
+        const arr = db.oplist
+        return arr.length ? arr[arr.length - 1] : null
+    }
+
+    function entry_equals(obj) {
+        const last = last_entry()
+        return expect(last).toStrictEqual(obj)
+    }
+
+    function with_autoinc(obj, autoinc?) {
+        if (autoinc != undefined) {
+            obj['autoinc'] = autoinc
+        }
+        return obj
+    }
+
+    function add(obj) {
+        return {add: obj}
+    }
+
+    beforeAll(() => {
+        db = new h.HullsDB('testdb')
+        return db
+    })
+
+    test('oplist after instantiation', () => {
+        expect(db.oplist).toHaveLength(0)
+    })
+
+    test('last_entry helper correctly set', () => {
+        expect(last_entry()).toBeNull()
+    })
+
+    test('add table with only name provided', () => {
+        db.create_table({name: 'onlyname'})
+        entry_equals(add({name: 'onlyname'}))
+    })
+
+    test.each([true, false])
+             ('create table with name and autoinc %s', (ainc) => {
+        const ent = {name: `name+inc${ainc}`, autoinc: ainc}
+        // this calls don't change added entry yet
+        db.create_table(ent)
+        entry_equals(add(ent))
+    })
+
+    test.each([undefined, true, false])
+             ('create table with string key and autoinc %s', (ainc) => {
+        const entry = with_autoinc({name: `strkey+${ainc}`, key: 'astring'}, ainc)
+        db.create_table(entry)
+        entry_equals(add(entry))
+    })
+
+    test.each([undefined, true, false])
+             ('create table with empty array key and autoinc %s', (ainc) => {
+        const entry = with_autoinc({name: `[]key+${ainc}`}, ainc)
+        db.create_table(entry)
+        entry_equals(add(entry))
+    })
+
+    test.todo('create table with array key and *separate* autoinc %s')
+
+    test.each([undefined, true])
+             ('create table with key: array *containing* AUTOINC and separate autoinc %s', (ainc) => {
+        const tname = '[..., AUTOINC]'
+        const keys = ['one', 'two']
+        // here AUTOINC should get removed from keys
+        const entry = with_autoinc({name: tname, key: [...keys, h.AUTOINC]}, ainc)
+        db.create_table(entry)
+        entry_equals(add({name: tname, key: keys, autoinc: true}))
+    })
+})
+// const db = new h.HullsDB('mydb');
+// db.create_table({name: 'nokey'});
+// db.create_table({name: 'nokeynoinc', autoinc: false});
+// db.create_table({name: 'nokey+inc', autoinc: true});
+// db.create_table({name: 'str', key: 'noinchere'});
+// db.create_table({name: 'strnoinc', key: 'dontincme', autoinc: false});
+// db.create_table({name: 'str+inc', key: 'incme', autoinc: true});
+// db.create_table({name: 'arr[]', key: []});
+// db.create_table({name: 'arr[..]', key: ['one', 'two']});
+// db.create_table({name: 'arr[..]+inc', key: ['one', 'two'], autoinc: true});
+// db.create_table({name: 'arr[.., AUTOINC]', key: ['one', 'two', h.AUTOINC]});
+// db.create_table({name: 'AUTOINC', key: h.AUTOINC});
 //db.create_table({name: 'errAUTOINC', key: h.AUTOINC, autoinc: false});
 //db.create_table({name: 'errarr[.., AUTOINC]', key: ['one', 'two', h.AUTOINC], autoinc: false});
-console.log(`Result:\n${db.oplist}`);
-console.table(db.oplist);
+//console.log(`Result:\n${db.oplist}`);
+//console.table(db.oplist);
